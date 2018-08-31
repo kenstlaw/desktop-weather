05bc4375d0e2be2373a30c8a315b3657e51bc146
Ken St. Lawrence <kstlawrence@gmail.com>
2018-08-30 22:06:42 -0400

    added a config files
    added the configparser module to use a config file.
    
    Cleaned up old debug lines

--- weather.py
+++ weather.py
@@ -1,26 +1,22 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 import requests
 import json
+from configparser import ConfigParser
 
+parser = ConfigParser()
+parser.read('desktop_weather.ini')
 
-WeatherUrl = "https://api.darksky.net/forecast/[api key]/42.568946,-71.265706"
+apikey = parser.get('Weather_Desktop','apikey')
+coords = parser.get('Weather_Desktop','coord')
+
+WeatherUrl = "https://api.darksky.net/forecast/" + apikey + "/" + coords 
+
 
 WeatherRequests = requests.get(WeatherUrl,timeout=1.0)
 
-#print WeatherRequests.text
 WeatherResponse = json.loads(WeatherRequests.text)
 
-#print type(WeatherResponse)
-
-#for key in WeatherResponse:
-#   print key
-
-#print type(WeatherResponse['currently'])
-
-#for key in WeatherResponse['currently']:
-#   print key
 CurrentWeather = WeatherResponse['currently']
-
 
 Humidity = CurrentWeather['humidity'] * 100
 
@@ -29,3 +25,5 @@ print ("Current Tempeture    : %3.0fF"% CurrentWeather['temperature'])
 print ("Dew Point            : %3.0fF"% CurrentWeather['dewPoint'])
 print ("Current Humidity     : %3.0f%%"% Humidity)
 print ("Corrent Conditions   :", CurrentWeather['summary'])
+
+exit(0)
